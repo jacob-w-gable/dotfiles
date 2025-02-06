184486ca317ab93e9a05a3234be80a9e837241a9
Jacob Gable <jacob.w.gable+pro@protonmail.com>
2025-02-06 02:09:23 -0600

    Add gnome-keyring to .zshrc

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -13,7 +13,9 @@ sudo apt install xorg sddm awesome \
   zathura \
   firefox \
   eom \
-  luarocks -y
+  luarocks \
+  gnome-keyring \
+  libsecret-tools -y
 
 # For building i3loc-color:
 sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
